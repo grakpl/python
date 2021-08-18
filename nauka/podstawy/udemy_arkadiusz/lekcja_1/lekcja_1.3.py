7dced1c47f9fe77cc1590b61951a579a4315ebc9
Grzegorz Rak <rak.grzegorz@interia.pl>
2021-08-19 01:12:51 +0200

    utworzenie lekcja_1.2.py - komentarze

--- nauka/podstawy/udemy_arkadiusz/lekcja_1/lekcja_1.3.py
+++ nauka/podstawy/udemy_arkadiusz/lekcja_1/lekcja_1.3.py
@@ -1,1 +1,8 @@
-#
+#PROGRAM DODAJĄCY DWIE LICZBY I POKAZUJĄCY SUME
+
+a = 5 # stworzenie zmiennej 'a' o wartości 5
+b = 2 # stworzenie zmiennej 'b' o wartości 2
+
+suma = a + b # stworzenie zmiennej suma z wunikiem dumowania 'a' i 'b'
+
+print(suma) # wypisanie wartości zmiennej 'suma'
