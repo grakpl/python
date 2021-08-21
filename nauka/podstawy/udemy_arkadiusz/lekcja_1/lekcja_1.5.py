6f5d1844fed8d80c939e901a8740dc726ae502ef
Grzegorz Rak <rak.grzegorz@interia.pl>
2021-08-22 00:52:28 +0200

    update lekcja_1.5.py -  dodano zmienn w różnych typach z komentarzem

--- nauka/podstawy/udemy_arkadiusz/lekcja_1/lekcja_1.5.py
+++ nauka/podstawy/udemy_arkadiusz/lekcja_1/lekcja_1.5.py
@@ -1,1 +1,15 @@
 # zmiennne typu float
+
+a = 5 #1. liczba całokwita int (integer)
+
+b = 4.3 # liczba zmiennoprzecinkowa (float) z kropką nie przecinkiem
+
+imie = "Grzegorz"
+
+zdanie = "I'm from Polend"
+
+zdanie2 = 'I\'m from Polend' # Znak \ informuje, że po nimmożna wyświetlić po nim dowolny znak
+
+czyDyzy = True #bool boolen False/True
+
+
